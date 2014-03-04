05e43cdcf38271b53ea89989731178f2d84cab32
CaffGeek <gibble@gmail.com>
2014-03-04 17:37:01 -0600

    first commit

--- src/com/caffeinatedgeek/strixe/scoretracker/MainActivity.java
+++ src/com/caffeinatedgeek/strixe/scoretracker/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.widget.Button.*;
 
 public class MainActivity extends Activity
 {
@@ -13,5 +14,24 @@ public class MainActivity extends Activity
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		Button leftTwoPinButton = (Button) findViewById(R.id.leftTwoPinButton);
+		Button leftThreePinButton = (Button) findViewById(R.id.leftThreePinButton);
+		Button headPinButton = (Button) findViewById(R.id.headPinButton);
+		Button rightThreePinButton = (Button) findViewById(R.id.rightThreePinButton);
+		Button rightTwoPinButton = (Button) findViewById(R.id.rightTwoPinButton);
+
+		leftTwoPinButton.setOnClickListener(pinClickHandler);
+		leftThreePinButton.setOnClickListener(pinClickHandler);
+		headPinButton.setOnClickListener(pinClickHandler);
+		rightThreePinButton.setOnClickListener(pinClickHandler);
+		rightTwoPinButton.setOnClickListener(pinClickHandler);
     }
+	
+	View.OnClickListener pinClickHandler = new View.OnClickListener() {
+		public void onClick(View v) {
+			Button button = (Button)v;
+			//Toast.makeText(getApplicationContext(), button.getText(), Toast.LENGTH_SHORT).show();
+		}
+	};
 }
